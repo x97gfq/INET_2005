d93cf8071b21b3acef35fd6b5b03080f21f3edc0
Jamie Symonds <jamie.symonds@gmail.com>
2025-09-24 21:48:46 -0300

    added examples for week 4

---
+++ nodejs/activity0/magic8ball.js
@@ -0,0 +1,16 @@
+
+
+/*
+
+const responses = [
+  "It is certain.",
+  "Without a doubt.",
+  "You may rely on it.",
+  "Ask again later.",
+  "Cannot predict now.",
+  "Don't count on it.",
+  "My reply is no.",
+  "Very doubtful."
+];
+
+*/
---
+++ nodejs/activity0/password_strength.js
@@ -0,0 +1,17 @@
+
+/*
+
+check length >= 8 characters & use regex to check for
+
+/[A-Z]/ - uppercase
+/[a-z]/ - lowercase
+/[0-9]/ - numbers
+/[^A-Za-z0-9]/ - special characters
+
+score 1 point for each
+
+< 3: weak
+3 or 4: moderate
+5: strong
+
+*/
